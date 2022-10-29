4e38f89c77a2e30a87b5e33941835df9db41ba73
DenisLedovskiy <BigDen_07@bk.ru>
2022-10-29 15:10:45 +0300

    first commit

---
+++ MapAppGB/MapViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapAppGB
+//
+//  Created by Денис Ледовский on 29.10.2022.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
